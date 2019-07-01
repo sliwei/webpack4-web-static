315f4726600f934e8c4b08ed8b7d82455e3c1a84
wei.li <274635143@qq.com>
2019-07-01 15:25:07 +0800

    基于webpack4的静态前端项目

---
+++ src/js/page.js
@@ -0,0 +1,11 @@
+"use strict"
+// import './page.scss'
+require('../scss/page.scss')
+import $ from 'jquery'
+import _ from 'lodash'
+
+window.onload = function () {
+	var text = $('#text')
+	console.log(text.html)
+	text.text = _.upperCase(text.text)
+}
